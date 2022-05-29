178ce1c69b699a0aec1e9e5fe62428fe5b08ec1b
Soji Takahashi <6687653+SofPyon@users.noreply.github.com>
2022-05-29 08:49:21 +0000

    共通のログインフォームを別ファイルに切り出し

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -45,58 +45,7 @@
                 <p class="jumbotron__lead">
                     {{ config('portal.admin_name') }}
                 </p>
-                <form method="post" action="{{ route('login') }}">
-                    @csrf
-
-                    @if ($errors->any())
-                        <div class="text-danger">
-                            @foreach ($errors->all() as $error)
-                                <p>{{ $error }}</p>
-                            @endforeach
-                        </div>
-                    @endif
-
-                    <div class="form-group">
-                        <label for="login_id"
-                            class="sr-only">{{ config('portal.student_id_name') }}または連絡先メールアドレス</label>
-                        <input id="login_id" type="text" class="form-control" name="login_id" value="{{ old('login_id') }}"
-                            required autocomplete="username" autofocus
-                            placeholder="{{ config('portal.student_id_name') }}または連絡先メールアドレス">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="password" class="sr-only">パスワード</label>
-                        <input id="password" type="password" class="form-control" name="password" required
-                            autocomplete="current-password" placeholder="パスワード">
-                    </div>
-
-                    <div class="form-group">
-                        <div class="form-checkbox">
-                            <label class="form-checkbox__label">
-                                <input class="form-checkbox__input" type="checkbox" name="remember" id="remember"
-                                    {{ old('remember') ? 'checked' : '' }}>
-                                ログインしたままにする
-                            </label>
-                        </div>
-                    </div>
-
-                    <p>
-                        <a href="{{ route('password.request') }}">
-                            パスワードをお忘れの場合はこちら
-                        </a>
-                    </p>
-
-                    <div class="form-group">
-                        <button type="submit" class="btn is-primary is-block">
-                            <strong>ログイン</strong>
-                        </button>
-                    </div>
-                    <p>
-                        <a class="btn is-secondary is-block" href="{{ route('register') }}">
-                            はじめての方は新規ユーザー登録
-                        </a>
-                    </p>
-                </form>
+                @include('includes.login_form')
             </app-container>
         </header>
     @endguest
@@ -120,8 +69,7 @@
                     <list-view-card>
                         <app-chips-container>
                             @foreach ($pinned_page->documents as $document)
-                                <app-chip href="{{ route('documents.show', ['document' => $document]) }}"
-                                    target="_blank">
+                                <app-chip href="{{ route('documents.show', ['document' => $document]) }}" target="_blank">
                                     <template v-slot:icon>
                                         @if ($document->is_important)
                                             <i class="fas fa-exclamation-circle fa-fw text-danger"></i>
---
+++ resources/views/includes/login_form.blade.php
@@ -0,0 +1,50 @@
+<form method="post" action="{{ route('login') }}">
+    @csrf
+
+    @if ($errors->any())
+        <div class="text-danger">
+            @foreach ($errors->all() as $error)
+                <p>{{ $error }}</p>
+            @endforeach
+        </div>
+    @endif
+
+    <div class="form-group">
+        <label for="login_id" class="sr-only">{{ config('portal.student_id_name') }}または連絡先メールアドレス</label>
+        <input id="login_id" type="text" class="form-control" name="login_id" value="{{ old('login_id') }}" required
+            autocomplete="username" autofocus placeholder="{{ config('portal.student_id_name') }}または連絡先メールアドレス">
+    </div>
+
+    <div class="form-group">
+        <label for="password" class="sr-only">パスワード</label>
+        <input id="password" type="password" class="form-control" name="password" required
+            autocomplete="current-password" placeholder="パスワード">
+    </div>
+
+    <div class="form-group">
+        <div class="form-checkbox">
+            <label class="form-checkbox__label">
+                <input class="form-checkbox__input" type="checkbox" name="remember" id="remember"
+                    {{ old('remember') ? 'checked' : '' }}>
+                ログインしたままにする
+            </label>
+        </div>
+    </div>
+
+    <p>
+        <a href="{{ route('password.request') }}">
+            パスワードをお忘れの場合はこちら
+        </a>
+    </p>
+
+    <div class="form-group">
+        <button type="submit" class="btn is-primary is-block">
+            <strong>ログイン</strong>
+        </button>
+    </div>
+    <p>
+        <a class="btn is-secondary is-block" href="{{ route('register') }}">
+            はじめての方は新規ユーザー登録
+        </a>
+    </p>
+</form>
